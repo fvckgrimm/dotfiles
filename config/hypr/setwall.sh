259bcb3095274391f82eb00c9d2034bf808bc2d4
Grimm <fvckgrimm@protonmail.ch>
2024-05-29 16:27:31 -0700

    Change wallpaper to swww
    wallpapers mix of whats in my repo and from walls from TorchedSammy on
    here

---
+++ config/hypr/setwall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+swww init &
+swww img -o "DP-1" ~/Pictures/walls/wallhaven-d6qwkg.jpg
+#swww img -o "DP-1" ~/git/walls/scenery.jpg
+
+swww img -o "HDMI-A-1" ~/.config/wallpapers/phone/1600843823067.png
+#swww img -o "HDMI-A-1" ~/.config/wallpapers/phone/1597992378910.png
+#swww img -o "HDMI-A-1" ~/.config/wallpapers/phone/1601951927319.png
